4ebdc4f28d7f60879e110f3fa1f18d316513667c
Ninad <telangn@gmail.com>
2017-12-27 09:12:11 -0800

    updated appium, updated resources

--- appiumSeleniumCucumberAndroid/src/test/java/mobileTest/appiumSeleniumCucumberAndroid/StepDefinitions.java
+++ appiumSeleniumCucumberAndroid/src/test/java/mobileTest/appiumSeleniumCucumberAndroid/StepDefinitions.java
@@ -1,49 +1,40 @@
 package mobileTest.appiumSeleniumCucumberAndroid;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import Utilities.AppiumServerJava;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 
 public class StepDefinitions {
 
-	File app = new File("/home/ninad/eclipse-workspace/aa_apk/app-qa.apk");
-
-	DesiredCapabilities dc = new DesiredCapabilities();
-	public static WebDriver driver = null;
+	AppiumServerJava appiumServer = new AppiumServerJava();
+	AppiumDriver<MobileElement> driver;
 
 	@Before("@setup")
 	public void setUp() {
 
-		dc.setCapability("BROWSER_NAME", "Android");
-		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-		dc.setCapability("deviceName", "nexus5");
-
-		dc.setCapability("app", app.getAbsolutePath());
-		dc.setCapability("appPackage", "com.aa.android.qa");
-		dc.setCapability("appActivity", "com.aa.android.view.SplashActivity");
-
+		int port = 4723;
 		try {
-			driver = new RemoteWebDriver(new URL("http://0.0.0.0:4723/wd/hub"), dc);
-			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		} catch (MalformedURLException e) {
+			if (!appiumServer.checkIfServerIsRunnning(port)) {
+				appiumServer.startServer();
+				appiumServer.stopServer();
+			} else {
+				System.out.println("Appium Server already running on Port - " + port);
+			}
+		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
 
+		driver = appiumServer.startServer();
 	}
 
 	@After("@destroy")
 	public void tearDown() throws Throwable {
 		driver.close();
+		appiumServer.stopServer();
 	}
 
 	@Given("^I log into the app$")
@@ -53,5 +44,4 @@ public class StepDefinitions {
 		driver.findElement(By.id("com.aa.android.qa:id/password")).sendKeys("testing");
 		driver.findElement(By.id("com.aa.android.qa:id/login_button")).click();
 	}
-
 }
